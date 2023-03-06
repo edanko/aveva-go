424a4d83bbc1169217b78c49733081611abfb62d
Egor Danko <qwertyone@gmail.com>
2023-03-07 01:33:51 +0300

    Add profiles count reading
    This will be used for capacity size later.

--- common_data.go
+++ common_data.go
@@ -2,6 +2,7 @@ package gen
 
 import (
 	"bufio"
+	"strconv"
 	"strings"
 )
 
@@ -9,6 +10,7 @@ type CommonData struct {
 	Shape     string
 	Dimension string
 	Quality   string
+	NoOfProfs uint64
 }
 
 func readCommonData(s *bufio.Scanner) *CommonData {
@@ -40,6 +42,9 @@ next:
 
 		case "QUALITY":
 			cd.Quality = v
+
+		case "NO_OF_PROFS":
+			cd.NoOfProfs, _ = strconv.ParseUint(v, 10, 64)
 		}
 	}
 	return cd
